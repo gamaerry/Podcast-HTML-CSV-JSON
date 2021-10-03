fafb3b5b7083d5d76dee965850cfed95d8b1cbde
luis <gamaerry@gmail.com>
2021-10-03 04:26:16 -0500

    Agregadas las portadas para cada episodio

--- js/obtener-contenido.js
+++ js/obtener-contenido.js
@@ -1,71 +1,79 @@
-//var d=[{'edad':'1','cm':'23','pais':'M','genero':'M','Q1':'10','Q2':'10'},{'edad':'2','cm':'34','pais':'M','genero':'M','Q1':'10','Q2':'10'},{'edad':'3','cm':'32','pais':'M','genero':'H','Q1':'10','Q2':'10'},]
-var csv='https://docs.google.com/spreadsheets/d/e/2PACX-1vTkhr9IVTzNtQmAbvjTQLcX4W3AkqTza140u83ir7zbu59NaD2rqfrba4QSmvF8Os8HVmZzZ8bT_7rC/pub?output=csv'
-var json= new Promise((resolve,reject)=>{
-  Papa.parse(csv, {
-    download: true,
-    header: true,
-    complete: function(resultados) {
-    	resolve(resultados.data)
-    }
-  })
-})
-json.then( function(valor){construir(valor)} )
+let json=Papa.parse(csv,{header: true})
+construir(json.data)
 function construir(datos){
   // Preparacion de elementos:
-  var tabla=document.getElementById('tabla')
-  var lista=document.getElementById('lista')
-  var titulo=document.getElementById('titulo')
-  var fechaAutor=document.getElementById('fecha-autor')
-  var player1=document.getElementById('player1')
+  let tabla=document.getElementById('tabla')
+  let lista=document.getElementById('lista')
+  let titulo=document.getElementById('titulo')
+  let fechaAutor=document.getElementById('fecha-autor')
+  let player1=document.getElementById('player1')
+  let fecha=datos[datos.length-1].fecha.split('/')
+
   //LLenado de la portada:
-  var fecha=datos[datos.length-1].Fecha.split('/')
-  switch (fecha[1]) {
-    case '1': fecha[1]='Enero'
-      break;
-    case '2': fecha[1]='Febrero'
-      break;
-    case '3': fecha[1]='Marzo'
-      break;
-    case '4': fecha[1]='Abril'
-      break;
-    case '5': fecha[1]='Mayo'
-      break;
-    case '6': fecha[1]='Junio'
-      break;
-    case '7': fecha[1]='Julio'
-      break;
-    case '8': fecha[1]='Agosto'
-      break;
-    case '9': fecha[1]='Septiembre'
-      break;
-    case '10': fecha[1]='Octubre'
-      break;
-    case '11': fecha[1]='Noviembre'
-      break;
-    default: fecha[1]='Diciembre'
-  }
-  titulo.innerHTML+=`Episodio ${datos[datos.length-1].Episodio}: ${datos[datos.length-1].Título}`
-  fechaAutor.innerHTML+=`<span class="text-white-opacity-05"><small>Por Luis Gerardo | ${fecha[1]} ${fecha[2]} </small ></span>`
-  player1.innerHTML+=`<source src="audio/${datos[datos.length-1].Título}.${datos[datos.length-1].Formato}" type="audio/${datos[datos.length-1].Formato}">`
-  // Lista y tabla
-  for (var i = 0; i < datos.length; i++) {
+  titulo.innerHTML+=`Episodio ${datos[datos.length-1].episodio}: ${datos[datos.length-1].titulo}`
+  fechaAutor.innerHTML+=`<span class="text-white-opacity-05"><small>Por Luis Gerardo | ${mes(fecha[1])} ${fecha[2]} </small ></span>`
+  player1.innerHTML+=`<source src="audio/${datos[datos.length-1].titulo}.${datos[datos.length-1].formato}" type="audio/${type(datos[datos.length-1].formato)}">`
+
+  for (let i = 0; i < datos.length; i++) {
     //Llenado de la lista:
-    lista.innerHTML+=`<li><a href="audio/${datos[i].Título}.${datos[i].Formato}">${datos[i].Episodio}. ${datos[i].Título}</a><\li>`
+    lista.innerHTML+=`<li><a href="audio/${datos[i].titulo}.${datos[i].formato}">${datos[i].episodio}. ${datos[i].titulo}</a><\li>`
     //Llenado de la tabla:
-    var descripcion= datos[i].Descripción
-        .replace(/\*\*([^\*]+)\*\*/g,'<a href="$1" target="_blank">$1</a>')
-        .replace(/\n/g,'<br>')
     tabla.innerHTML+=`
       <tr>
-				<td>${datos[i].Episodio}</td>
-				<td>${datos[i].Título}</td>
+				<td>${formatoIMG(datos[i].episodio, datos[i].imagen)}</td>
+				<td>${datos[i].titulo}</td>
 				<td>
-				  <audio id="player2" preload="none" style="max-width: 100%">
-					  <source src="audio/${datos[i].Título}${datos[i].Formato}">
+				  <audio id="player2" controls style="min-width: 100%">
+					  <source src="audio/${datos[i].titulo}.${datos[i].formato}" type="audio/${type(datos[i].formato)}">
 				  </audio><br>
-				  ${descripcion}
+				  ${formatoHTML(datos[i].descripcion)}
 				</td>
-				<td>${datos[i].Fecha}</td>
+				<td>${datos[i].fecha}</td>
       </tr>`
   }
+}
+function mes(numero){
+  switch (numero) {
+    case '1': numero='Enero'
+      break;
+    case '2': numero='Febrero'
+      break;
+    case '3': numero='Marzo'
+      break;
+    case '4': numero='Abril'
+      break;
+    case '5': numero='Mayo'
+      break;
+    case '6': numero='Junio'
+      break;
+    case '7': numero='Julio'
+      break;
+    case '8': numero='Agosto'
+      break;
+    case '9': numero='Septiembre'
+      break;
+    case '10': numero='Octubre'
+      break;
+    case '11': numero='Noviembre'
+      break;
+    default: numero='Diciembre'
+  }
+  return numero
+}
+function type(formato){
+  if (formato=='wav'||formato=='ogg') {
+    return formato
+  }else {
+    return 'mpeg'
+  }
+}
+function formatoHTML(descripcion){
+  return descripcion
+      .replace(/(^[^\s]+ [^\s]+:)/,'<span style="font-weight: bold">$1</span>') //ojo: aquí no es global
+      .replace(/\*\*([^*]+)\*\*/g,'<a href="$1" target="_blank">$1</a>')
+      .replace(/\n/g,'<br>')
+}
+function formatoIMG(episodio, imagen){
+	return episodio
+		.replace(/(\d+)/, `$1 <br><a href="images/${imagen}" target="_blank">(ver portada)</a>`)
 }
